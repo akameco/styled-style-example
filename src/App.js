ea6e83cea7d8f9c1be40c5f5b02e693ee42b0e85
akameco <akameco.t@gmail.com>
2018-02-05 18:50:20 +0900

    chore(npm): update packages

--- src/App.js
+++ src/App.js
@@ -13,20 +13,18 @@ const Title = h1('title')
 const Intro = p('intro')
 const Logo = img('logo')
 
-class App extends React.Component<{}> {
-  render() {
-    return (
-      <Wrapper>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <Title>Welcome to React</Title>
-        </Header>
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
-      </Wrapper>
-    )
-  }
+function App() {
+  return (
+    <Wrapper>
+      <Header>
+        <Logo src={logo} alt="logo" />
+        <Title>Welcome to React</Title>
+      </Header>
+      <Intro>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </Intro>
+    </Wrapper>
+  )
 }
 
 export default App
